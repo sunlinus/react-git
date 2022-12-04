1653b6c0a30bafd2c996d2666d21e53b256a37dd
sunlinus <119735633+sunlinus@users.noreply.github.com>
2022-12-04 20:30:19 +0530

    second change in pagenotfound

--- src/components/Routing/PageNotFound.js
+++ src/components/Routing/PageNotFound.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function PageNotFound() {
-  return <div>PageNotFound here aswell</div>;
+  return <div>PageNotFound here aswell in dev-2</div>;
 }
 
 export default PageNotFound;
